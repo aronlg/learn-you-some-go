27a1bffc3332447296d2cb962569a26eddce3411
aronlg <aronlgreen@gmail.com>
2022-03-14 21:15:16 +0000

    Use greetingPrefix func

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -13,16 +13,7 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
-
-	switch language {
-	case french:
-		prefix = frenchHelloPrefix
-	case spanish:
-		prefix = spanishHelloPrefix
-	}
-
-	return prefix + name
+	return greetingPrefix(language) + name
 }
 
 func greetingPrefix(language string) (prefix string) {
