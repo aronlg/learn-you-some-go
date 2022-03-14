e6a9327f199db6d33cbe9249832550379fcc0182
aronlg <aronlgreen@gmail.com>
2022-03-14 20:59:26 +0000

    Refactor magic values and prefix logic; use switch

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,22 +2,39 @@ package main
 
 import "fmt"
 
+const french = "French"
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Hola, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == "French" {
-		return "Bonjour, " + name
+	prefix := englishHelloPrefix
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
 	}
 
-	if language == "Spanish" {
-		return "Hola, " + name
-	}
+	return prefix + name
+}
 
-	return englishHelloPrefix + name
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
