f3a8eeef0399bb366f06f4f6cc530dd783d83856
aronlg <aronlgreen@gmail.com>
2022-03-15 12:16:48 +0000

    Refactor Sum to accept a variable length slices

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,17 +1,9 @@
 package sum
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	/*
-		for i := 0; i < 5; i++ {
-			sum += numbers[i]
-		}
-	*/
-
-	// Refactored using range keyword
 	for _, number := range numbers {
 		sum += number
 	}
-
 	return sum
 }
