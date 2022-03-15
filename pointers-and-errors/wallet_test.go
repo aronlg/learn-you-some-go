ec9931691079c2770b2fdd350693131cd13f7a76
aronlg <aronlgreen@gmail.com>
2022-03-15 15:55:38 +0000

    Create Withdraw method

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -1,22 +1,34 @@
 package wallet
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	got := wallet.Balance()
+		wallet.Deposit(Bitcoin(10))
 
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+		got := wallet.Balance()
 
-	want := 10
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
