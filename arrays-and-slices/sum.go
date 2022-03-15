0ddd12dc254512cba208c4d9283211e2e633a185
aronlg <aronlgreen@gmail.com>
2022-03-15 14:31:48 +0000

    Fix SumAllTails when a slice length is 0

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -8,22 +8,15 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
-	}
-
-	return sums
-}
-
 func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range numbersToSum {
-		tail := numbers[1:]
-		sums = append(sums, Sum(tail))
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
