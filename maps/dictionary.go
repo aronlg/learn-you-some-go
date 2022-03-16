f3d42ddd0aed400a1e2d0855a0c32d9941d9b86e
Aron Green <aron@ccpgames.com>
2022-03-16 11:11:43 +0000

    Clean up Add method test

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -6,8 +6,6 @@ type Dictionary map[string]string
 
 var ErrNotFound = errors.New("could not find the word you were looking for")
 
-// var dictionary = map[string]string{}
-
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
