24ef38edeacd304496b7cf0bc0e23c4d72d9b276
Aron Green <aron@ccpgames.com>
2022-03-16 11:03:17 +0000

    Return an error if word not found in dict

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -22,6 +22,12 @@ func TestSearch(t *testing.T) {
 
 		assertStrings(t, err.Error(), want)
 	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -29,5 +35,13 @@ func assertStrings(t testing.TB, got, want string) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
